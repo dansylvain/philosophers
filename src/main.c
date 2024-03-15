d677606a46cc65a8afb9db8439e1e05d1699f943
dansylvain <dansylvain84@gmail.com>
2024-03-15 07:41:59 +0100

    created func timeval to ms

--- src/main.c
+++ src/main.c
@@ -6,11 +6,16 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 16:54:14 by dan               #+#    #+#             */
-/*   Updated: 2024/03/15 07:34:36 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/15 07:41:08 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+long	timeval_to_ms(struct timeval time_struct)
+{
+	return (time_struct.tv_sec * 1000 + time_struct.tv_usec / 1000);
+}
+
 
 void	*filo_routine(void *arg)
 {
@@ -26,7 +31,7 @@ void	*filo_routine(void *arg)
 	
 	// display starting time
 	pthread_mutex_lock(&filo->data->print_mutex);
-	printf("%i: time now: %ld\n", filo->id, start.tv_sec * 1000 + start.tv_usec / 1000) ;
+	printf("%i: time now: %ld\n", filo->id, timeval_to_ms(start)) ;
 	pthread_mutex_unlock(&filo->data->print_mutex);
 	
 	// live or die loop 
@@ -34,7 +39,7 @@ void	*filo_routine(void *arg)
 	while (time_passed < filo->data->tt_die)
 	{
 	gettimeofday(&now, NULL);
-	time_passed = (now.tv_sec * 1000 + now.tv_usec / 1000) - (start.tv_sec * 1000 + start.tv_usec / 1000) ;
+	time_passed = (timeval_to_ms(now)) - (timeval_to_ms(start)) ;
 	}
 	pthread_mutex_lock(&filo->data->print_mutex);
 	printf("filo %i died\n", filo->id);
