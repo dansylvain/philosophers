cb9832861992d355a96c93d664aacf2c2e188bf6
dansylvain <dansylvain84@gmail.com>
2024-03-17 10:24:58 +0100

    added func xpress_mssg

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 08:45:27 by dan               #+#    #+#             */
-/*   Updated: 2024/03/17 10:04:13 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/17 10:24:16 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,43 +19,20 @@ int		check_input(int argc, char **argv);
 void	free_data(t_Data *data);
 void	display_error(char *str);
 int		create_and_initialize_data(t_Data **data, char **argv);
+t_Data	*run_threads(t_Data *data);
+void	xpress_mssg(t_filo *filo, mssg mssg);
 
-long	time_to_ms(struct timeval time_struct)
-{
-	return (time_struct.tv_sec * 1000 + time_struct.tv_usec / 1000);
-}
 
-void	xpress_mssg(long int t, int fil, mssg mssg, pthread_mutex_t *mut)
-{
-	char *mssg_str;
-	struct timeval	now;
-
-	if (mssg == take_fork)
-		mssg_str = "has taken a fork";
-	if (mssg == eating)
-		mssg_str = "is eating";
-	if (mssg == sleeping)
-		mssg_str = "is sleeping";
-	if (mssg == thinking)
-		mssg_str = "is thinking";
-	if (mssg == dead)
-		mssg_str = "died";	
-	pthread_mutex_lock(mut);
-	gettimeofday(&now, NULL);
-	if (t == 0)
-		t = time_to_ms(now);
-	printf("%li %i %s\n", t, fil, mssg_str);
-	pthread_mutex_unlock(mut);
-}
 
 void	*filo_routine(void *arg)
 {
 	t_filo *filo;
 
 	filo = (t_filo *)arg;
-	pthread_mutex_lock(&filo->data->print_mutex);
-	printf("%i: hello!\n", filo->id);
-	pthread_mutex_unlock(&filo->data->print_mutex);
+	// pthread_mutex_lock(&filo->data->print_mutex);
+	xpress_mssg(filo, eating);
+	// printf("%i: hello!\n", filo->id);
+	// pthread_mutex_unlock(&filo->data->print_mutex);
 	return (NULL);
 }
 
@@ -70,25 +47,31 @@ int main(int argc, char **argv)
 		return (free_data(data), display_error("Error\n"), 2);
 	
 	printf("welcome to the jungle\n");
-	// printf("%i, %i, %i, %i, %i\n", data->fil_nbr, data->tt_die, data->tt_eat, data->tt_sleep, data->max_meals);
+	data = run_threads(data);
+	if (data == NULL)
+		return (free_data(data), display_error("Error\n"), 3);
+	
+	pthread_mutex_destroy(&data->print_mutex);
+	free_data(data);
+	return (0);
+}
+
+t_Data *run_threads(t_Data *data)
+{
+	int	i;
 	
 	i = 0;
 	while (i < data->fil_nbr)
 	{
 		data->filo[i].id = i;
-		if(pthread_create(&data->filo[i].filo, NULL, filo_routine, &data->filo[i]) != 0)
-			return (free_data(data), display_error("Error\n"), 3);
-		i++;
+		if(pthread_create(&data->filo[i++].filo, NULL, filo_routine, &data->filo[i]) != 0)
+			return (NULL);
 	}
-	
 	i = 0;
 	while (i < data->fil_nbr)
 	{
-		if(pthread_join(data->filo[i].filo, NULL) != 0)
-			return (free_data(data), display_error("Error\n"), 4);
-		i++;
+		if(pthread_join(data->filo[i++].filo, NULL) != 0)
+			return (NULL);
 	}
-	pthread_mutex_destroy(&data->print_mutex);
-	free_data(data);
-	return (0);
+	return (data);	
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 07:27:02 by dan               #+#    #+#             */
-/*   Updated: 2024/03/17 09:57:41 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/17 10:23:29 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "libft.h"
 #include <stdio.h>
 #include <errno.h>
+# include <sys/time.h>
 
 void	free_data(t_Data *data)
 {
@@ -26,4 +27,34 @@ void	display_error(char *str)
 {
 	if (write (2, str, ft_strlen(str)) == -1)
 		perror("display_error");
+}
+
+long	time_to_ms(struct timeval time_struct)
+{
+	return (time_struct.tv_sec * 1000 + time_struct.tv_usec / 1000);
+}
+
+void	xpress_mssg(t_filo *filo, mssg mssg)
+{
+	char 			*mssg_str;
+	long int		t;
+	struct timeval	now;
+	pthread_mutex_t *mut;
+
+	if (mssg == take_fork)
+		mssg_str = "has taken a fork";
+	if (mssg == eating)
+		mssg_str = "is eating";
+	if (mssg == sleeping)
+		mssg_str = "is sleeping";
+	if (mssg == thinking)
+		mssg_str = "is thinking";
+	if (mssg == dead)
+		mssg_str = "died";
+	mut = &filo->data->print_mutex;
+	pthread_mutex_lock(mut);
+	gettimeofday(&now, NULL);
+	t = time_to_ms(now);
+	printf("%li %i %s\n", t, filo->id, mssg_str);
+	pthread_mutex_unlock(mut);
 }
