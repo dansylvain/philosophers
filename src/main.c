9e27a28d24de8b650b8a3abad414e4c2114cd8dd
dansylvain <dansylvain84@gmail.com>
2024-03-21 20:58:03 +0100

    solved the problem

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dan <dan@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 08:45:27 by dan               #+#    #+#             */
-/*   Updated: 2024/03/21 20:05:12 by dan              ###   ########.fr       */
+/*   Updated: 2024/03/21 20:57:33 by dan              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,10 @@ void	destroy_mutexes(t_data *data);
  *========================================================================**/
 t_filo	*eat_and_sleep(t_filo *filo)
 {
+	pthread_mutex_lock(&filo->can_eat_mtx);
+	filo->can_eat = false;
+	pthread_mutex_unlock(&filo->can_eat_mtx);
+	get_time_now(&filo->meal_time);	
 	xpress_mssg(filo, eating);
 	usleep(filo->data->tt_eat * 1000);
 	xpress_mssg(filo, sleeping);
@@ -48,9 +52,11 @@ int	filo_can_eat(t_filo *filo)
 	
 	yes_or_no = 0;
 	pthread_mutex_lock(&filo->can_eat_mtx);
+	// printf("filo_can_eat % i\n", filo->id);
 	if (filo->can_eat == true)
 		yes_or_no = 1;
 	pthread_mutex_unlock(&filo->can_eat_mtx);
+	// printf("yes_or_no: %i\n", yes_or_no);
 	return (yes_or_no);
 }
 
@@ -64,6 +70,7 @@ void	*filo_rtn(void *arg)
 	xpress_mssg(filo, got_born);
 	while (time_now < filo->meal_time + filo->data->tt_die)
 	{
+		usleep(100);
 		if (filo_can_eat(filo))
 			filo = eat_and_sleep(filo);
 		get_time_now(&time_now);
@@ -81,13 +88,14 @@ void	*coor_rtn(void *arg)
 	while (1)
 	{
 		pthread_mutex_lock(&data->filo[0].can_eat_mtx);
-		data->filo[0].can_eat == true;
+		data->filo[0].can_eat = true;
+		// printf("data->filo[0].can_eat: %i\n", data->filo[0].can_eat);
 		pthread_mutex_unlock(&data->filo[0].can_eat_mtx);
 
 		// pthread_mutex_lock(&data->print_mtx);
 		// printf("%i: I'm watching you...\n", data->fil_nbr);
 		// pthread_mutex_unlock(&data->print_mtx);
-		i++;
+		// i++;
 	}
 	return (NULL);
 }
